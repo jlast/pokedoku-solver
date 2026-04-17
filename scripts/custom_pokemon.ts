2478e98665b0b76f02baba470f4c076def75baf7
Jeroen van der Last <jeroen.vander.last@essent.nl>
2026-04-17 13:33:25 +0200

    Fix list a bit more

--- scripts/custom_pokemon.ts
+++ scripts/custom_pokemon.ts
@@ -10,4 +10,41 @@ export const CUSTOM_POKEMON: Pokemon[] = [
     "region": "Kanto",
     "evolutionStage": "First Stage"
   }, 
+  {
+    "id": 449,
+    "name": "Hippopotas female",
+    "types": [
+      "Ground"
+    ],
+    "region": "Sinnoh",
+    "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/449.png",
+    "evolutionStage": "First Stage"
+  },
+  {
+    "id": 450,
+    "name": "Hippowdon female",
+    "types": [
+      "Ground"
+    ],
+    "region": "Sinnoh",
+    "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/450.png",
+    "evolutionStage": "Final Stage",
+    "evolutionTrigger": [
+      "Evolved by Level"
+    ]
+  },
+  {
+    "id": 521,
+    "name": "Unfezant female",
+    "types": [
+      "Normal",
+      "Flying"
+    ],
+    "region": "Unova",
+    "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/521.png",
+    "evolutionStage": "Final Stage",
+    "evolutionTrigger": [
+      "Evolved by Level"
+    ]
+  },
 ]
