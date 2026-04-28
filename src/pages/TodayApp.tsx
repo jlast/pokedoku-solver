15d0c328fc45546d9fba0879ca9004272d3b24e1
Pokedoku Helper <jeroen.vander.last@gmail.com>
2026-04-28 14:55:42 +0200

    Update InfoBox text for clarity

--- src/pages/TodayApp.tsx
+++ src/pages/TodayApp.tsx
@@ -348,7 +348,7 @@ export function TodayApp({ puzzle }: TodayAppProps) {
           onSwapClick={handleCellClick}
           onConstraintChange={() => {}}
         />
-        <InfoBox>Suggested Pokedoku answers highlighting rarer, harder Pokémon. Tap a square for all options.</InfoBox>
+        <InfoBox>These are suggested Pokedoku answers highlighting rarer, harder Pokémon. Tap a square for all options.</InfoBox>
         <div ref={suggestionsRef}>
           <SuggestionsPanel
             selectedCell={grid.selectedCell}
