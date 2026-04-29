69ebd67f3eca22e16b5851de4c0d075430bfb795
Pokedoku Helper <jeroen.vander.last@gmail.com>
2026-04-29 14:02:22 +0200

    remove unused files

--- src/pages/puzzle-stats.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import PuzzleStatsApp from "./PuzzleStatsApp";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <PuzzleStatsApp />
-  </StrictMode>,
-);
--- src/pages/today.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { TodayLoader } from './TodayLoader';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <TodayLoader />
-  </StrictMode>,
-)
