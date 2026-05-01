d9483e8ab775455c7cfb2f7b271c93f103f48f50
Pokedoku Helper <jeroen.vander.last@gmail.com>
2026-05-01 14:24:32 +0200

    Generate sitemap and add 404 page

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://www.pokedoku-helper.com",
   output: "static",
   publicDir: "../public",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
